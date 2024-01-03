ebf2b3d4c0d9c6dc67a119a8f35867e814938372
Jonas Schlösser <jonas.schloesser@gmx.de>
2024-01-03 17:56:07 +0100

    fixed layout in report and improved report intervals

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ console.log(`Script started at ${new Date()}`)
 cron.schedule('0 */5 * * * *', () => {
     checkStatus()
 })
-cron.schedule('0 */15 * * * *', () => {
+cron.schedule('0 0,15,30,45 * * * *', () => {
     report()
 })
 
@@ -36,7 +36,7 @@ async function checkStatus() {
 function report() {
     tmpdata = read()
     tmpdate = new Date()
-    console.log(`\n- Report ${tmpdate.getDate()}.${tmpdate.getMonth() + 1}.${tmpdate.getFullYear()} at ${tmpdate.getHours()}:${tmpdate.getMinutes()} -----------------\n`)
+    console.log(`\n- Report ${tmpdate.getDate()}.${tmpdate.getMonth() + 1}.${tmpdate.getFullYear()} at ${tmpdate.getHours()}:${tmpdate.getMinutes()} -------------------------------`)
     for(var i = 0; i < tmpdata.length; i++) {
         console.log(`   ${tmpdata[i].address}: ${tmpdata[i].last_status} ${tmpdata[i].last_status == 200 ? '(OK)' : '(Internal Server Error)'}`)
     }
